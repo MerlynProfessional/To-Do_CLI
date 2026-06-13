c169e06efe5a99b3eb26286e680abd74194ec092
workmerlyn@gmail.com <workmerlyn@gmail.com>
2026-06-13 19:44:40 +0800

    add date time format detection feature, and create table manually function

--- Function.py
+++ Function.py
@@ -2,7 +2,49 @@
 from tabulate import tabulate
 
 headers = ["Category", "Task", "Done", "CreatedAT", "CompletedAt"]
+
 def todo(PrepData):
     print(tabulate(PrepData, headers=headers, tablefmt="grid"))
+
+def todo_manual(PrepData):
+
+    length=[]
+    result=[]
+    a=0
+    while (a<len(PrepData)) :
+        length.append([len(PrepData[a][0]),len(PrepData[a][1]),len(PrepData[a][2]),len(str(PrepData[a][3])),len(str(PrepData[a][4]))])
+        a+=1
+    total=0
+    
+    for i in range(len(headers)) :
+        max=len(headers[i])
+        for j in range(len(length)) :
+            if length[j][i]>max :
+                max=length[j][i]
+        result.append(max)
+        total+=max
+        max=0   
+    print(result)
+     
+    total+=2*(((int(length[0][i])-len(headers[i]))//2))
+    print('='*(total+19))
+    print('||',end=' ')
+    for i in range(len(headers)) :
+        print(' '*((int(result[i])-len(headers[i]))//2),end='')
+        print(f"{headers[i]}", end=' ')
+        print(' '*((int(result[i])-len(headers[i]))//2),end='')
+        print(' ||',end=' ')
+    print()  
+    
+    for i in range(len(PrepData)) :
+        print('='*(total+19))
+        print('||',end=' ') 
+        for j in range(len(PrepData[i])) :
+            print(' '*((int(result[j])-len(str(PrepData[i][j])))//2),end='')
+            print(f"{PrepData[i][j]}", end=' ')
+            print(' '*((int(result[j])-len(str(PrepData[i][j])))-((int(result[j])-len(str(PrepData[i][j])))//2)),end='')
+            print(' ||',end=' ')
+    print()        
+    print('='*(total+19))
 
 # Define headers
--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 from Function import *
-import datetime
+from datetime import datetime
 
 
 count=int(input("How many to-do items do you want to add? "))
@@ -11,12 +11,24 @@ for j in range(count):
     for i in range(len(headers)):
         if headers[i] == "Done":
             PrepData[j].append('Done' if (input(f"{headers[i]} (True/False): "))=='True' else 'Not Done')
-       
-        else:
-            PrepData[j].append(input(f'{headers[i]}: '))
+        elif headers[i] == "CreatedAT":
+            try :
+                PrepData[j].append(datetime.strptime(input(f"{headers[i]} (YYYY-MM-DD): "), "%Y-%m-%d"))
+            except ValueError:
+                print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
+                exit()
+        elif headers[i] == "CompletedAt":
+            try :
+                PrepData[j].append(datetime.strptime(input(f"{headers[i]} (YYYY-MM-DD): "), "%Y-%m-%d"))
+            except ValueError:
+                print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
+                exit()
+        else :
+            PrepData[j].append(input(f"{headers[i]}: "))
 
     number-=1
 todo(PrepData)
+todo_manual(PrepData)
 
 # Print the table with a grid formatting style
 # print(tabulate(data, headers=headers, tablefmt="grid"))
